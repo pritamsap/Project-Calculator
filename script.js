073db0fa3eef03eaa44a016869da850904a37b58
PritamSapkota <slicefrost1@gmail.com>
2022-09-02 13:28:53 -0400

    Create a operate function
    	 Operate function takes three parameters - a operator, first number, and the second num. Inside the function, if/else conditions to check - which operator is the one being issued then calls one of the 4 basic function accordingly.  This operate function is going to act as the equal sign represented in calculator.

--- script.js
+++ script.js
@@ -23,6 +23,21 @@ function division(num1, num2) {
 
 
 //operate function
+function operate(operator, num1, num2) {
+    if(operator === "+") {
+        return addition(num1, num2);
+    }
+    else if(operator === "-") {
+        return subtraction(num1, num2);
+    }
+    else if(operator === "*") {
+        return multplication(num1, num2);
+    }
+
+    else {
+        return division(num1, num2);
+    }
+}
 
 
 
