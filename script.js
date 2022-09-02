44dea227f5e2b921913848f0c1a7c887e5f17202
PritamSapkota <slicefrost1@gmail.com>
2022-09-02 10:26:41 -0400

    Add basic calculator operator on js file
    		Added 4 basic calculator operator function on javascript file - add, sub, multiply and divide.
    These functions take two number as their parameters and return the operated value.

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+// basic math operators
+
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+
+function subtraction(num1, num2) {
+    return num1 - num2;
+}
+
+
+function multplication(num1, num2) {
+    return num1 * num2;
+}
+
+
+function division(num1, num2) {
+    return num1 / num2;
+}
+
+
+//operate function
+
+
+
+
+
+
+
