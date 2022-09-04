69cc6d6d49377c331ef9dfedd2dbd4ae36ef5b18
PritamSapkota <slicefrost1@gmail.com>
2022-09-04 11:34:12 -0400

    Implement calculator display to show input
    		Using query selectorall  numbers from 0 to 9 with their class name as 'number', are called within addEventListener. If a number is clicked,  for loop is used to check if the text content within the clicked number div matches  - else i increments until the number matches the text content and then calculator Display is updated with that pressed number.

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 
 
+const add = document.querySelector('.add');
+const equals = document.querySelector('.equals');
+const calculatorDisplay = document.querySelector('.calculator-display');
+
+
+
+
+
+
+
+
+
+
 // basic math operators
 function addition(num1, num2) {
     return num1 + num2;
 }
 
-
 function subtraction(num1, num2) {
     return num1 - num2;
 }
 
-
 function multplication(num1, num2) {
     return num1 * num2;
 }
-
 
 function division(num1, num2) {
     return num1 / num2;
@@ -32,11 +42,33 @@ function operate(operator, num1, num2) {
     else if(operator === "*") {
         return multplication(num1, num2);
     }
-
     else {
         return division(num1, num2);
     }
 }
+
+
+
+//number display
+const currentPressedNumber = document.querySelectorAll('.number');
+
+currentPressedNumber.forEach(number => {
+    number.addEventListener('click', () => {
+        for(let i = 0; i < 10; i++) {
+            if(number.textContent === `${i}`) {
+                calculatorDisplay.textContent = i;
+            }
+
+        }
+    });
+});
+
+
+
+
+
+
+
 
 
 
