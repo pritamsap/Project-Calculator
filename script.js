4e5f92237318963870407ae6a961149d0389f8cd
PritamSapkota <slicefrost1@gmail.com>
2022-09-05 09:50:46 -0400

    Update add and equals to display result
    		Three new variables are declared. A 'firstValue', 'lastValue' and isfirstNumber' boolean to switch between first and last value pressed. Initially isfirstNumber is true, so when user presses any number it will be stored in firstValue then once 'add'  div is  click 'isfirstNumber' turns false,  which then means that if users presses other number - it will be stored in 'lastValue' and finally
    With 'equals' div pressed, operate function is called and it's value is returned on the display.

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-
-
 const add = document.querySelector('.add');
 const equals = document.querySelector('.equals');
 const calculatorDisplay = document.querySelector('.calculator-display');
-
-
-
-
-
-
-
-
+const clear = document.querySelector('.clear');
+const pressedNumber = document.querySelectorAll('.number');
 
 
 // basic math operators
 function addition(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
-
 function subtraction(num1, num2) {
     return num1 - num2;
 }
-
 function multplication(num1, num2) {
     return num1 * num2;
 }
-
 function division(num1, num2) {
     return num1 / num2;
 }
@@ -33,42 +22,65 @@ function division(num1, num2) {
 
 //operate function
 function operate(operator, num1, num2) {
-    if(operator === "+") {
-        return addition(num1, num2);
+    let finalValue = 0;
+    if(operator === "add") {       
+        finalValue = addition(num1, num2);
     }
-    else if(operator === "-") {
-        return subtraction(num1, num2);
+    else if(operator === "sub") {
+        finalValue = subtraction(num1, num2);
     }
-    else if(operator === "*") {
-        return multplication(num1, num2);
+    else if(operator === "mul") {
+        finalValue = multplication(num1, num2);
     }
     else {
-        return division(num1, num2);
+        finalValue = division(num1, num2);
     }
+    return finalValue;
 }
 
+let firstValue = 0;
+let lastValue = 0;
+let isfirstNumber = true;
+pressedNumber.forEach(item => {
+    item.addEventListener('click', () => {
 
-
-//number display
-const currentPressedNumber = document.querySelectorAll('.number');
-
-currentPressedNumber.forEach(number => {
-    number.addEventListener('click', () => {
-        for(let i = 0; i < 10; i++) {
-            if(number.textContent === `${i}`) {
-                calculatorDisplay.textContent = i;
+        if(isfirstNumber) {
+            for(let i = 0; i < 10; i++) {
+                if(item.textContent == i) {
+                    calculatorDisplay.textContent += i;
+                    firstValue += "" + i;
+                }
             }
-
         }
+        else {
+            for(let i = 0; i < 10; i++) {
+                if(item.textContent == i) {
+                    calculatorDisplay.textContent += i;
+                    lastValue += "" + i;
+                }
+            }
+        }
+
     });
 });
 
 
+// add Events on Equals and operator expression
+add.addEventListener('click', () => {
+    calculatorDisplay.textContent = "";
+    isfirstNumber = false;
+})
 
+equals.addEventListener('click', () => {
+    calculatorDisplay.textContent = operate("add", firstValue, lastValue);
+    isfirstNumber = true;
+})
 
-
-
-
+clear.addEventListener('click', () => {
+    calculatorDisplay.textContent = 0;
+    firstValue = 0;
+    lastValue = 0;
+})
 
 
 
