d36ea8ef3aba376717a8f7ae610a87ae44d49e65
PritamSapkota <slicefrost1@gmail.com>
2022-09-08 16:52:34 -0400

    Update all operators buttons click event
    	Now the calculator evaluates one operation at the time. An operation object is declared with three attributes [firstValue, secondValue, & operator] which is assigned based on the number and operator pressed. This then can be used to compute the operation result. This sequence works for all four basic operators.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const add = document.querySelector('.add');
-const equals = document.querySelector('.equals');
+
+const equalBtn = document.querySelector('.equals');
 const calculatorDisplay = document.querySelector('.calculator-display');
-const clear = document.querySelector('.clear');
+const clearBtn = document.querySelector('.clear');
 const pressedNumber = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const powerButton = document.querySelector('.logo');
+const buttons = document.querySelector('.buttons');
+const calcBody = document.querySelector('.calculator');
 
 
-// basic math operators
+
+
+// basic math operator function
 function addition(num1, num2) {
     return Number(num1) + Number(num2);
 }
@@ -19,75 +25,127 @@ function division(num1, num2) {
     return num1 / num2;
 }
 
-
 //operate function
 function operate(operator, num1, num2) {
     let finalValue = 0;
-    if(operator === "add") {       
+    if(operator === 'add') {       
         finalValue = addition(num1, num2);
     }
-    else if(operator === "sub") {
+    else if(operator === 'sub') {
         finalValue = subtraction(num1, num2);
     }
-    else if(operator === "mul") {
+    else if(operator === 'mul') {
         finalValue = multplication(num1, num2);
     }
-    else {
+    else if(operator === 'div') {
         finalValue = division(num1, num2);
     }
     return finalValue;
 }
 
-let firstValue = 0;
-let lastValue = 0;
-let isfirstNumber = true;
+
+let displayValue = '0';
+let isbuttonPressed = false;
+calculatorDisplay.textContent = displayValue;
+
 pressedNumber.forEach(item => {
     item.addEventListener('click', () => {
-
-        if(isfirstNumber) {
-            for(let i = 0; i < 10; i++) {
-                if(item.textContent == i) {
-                    calculatorDisplay.textContent += i;
-                    firstValue += "" + i;
-                }
+        for(let i = 0; i < 10; i++) {
+            if(displayValue === '0') {
+                displayValue = "";
             }
+            if(item.textContent == i) {
+             displayValue += `${i}`;   
+            }
+        }
+        calculatorDisplay.textContent = displayValue;
+        if(isbuttonPressed == false) {
+            operation.firstValue = displayValue;
+
         }
         else {
-            for(let i = 0; i < 10; i++) {
-                if(item.textContent == i) {
-                    calculatorDisplay.textContent += i;
-                    lastValue += "" + i;
-                }
-            }
-        }
+            operation.secondValue = displayValue;
 
+        }
     });
 });
 
+// object operation
+let operation = {firstValue: undefined, operator: undefined, secondValue: undefined};
 
-// add Events on Equals and operator expression
-add.addEventListener('click', () => {
-    calculatorDisplay.textContent = "";
-    isfirstNumber = false;
+operatorBtns.forEach(b => {
+    b.addEventListener('click', () => {
+        if(operation.firstValue !== undefined && operation.secondValue !== undefined) {
+            displayValue = operate(operation.operator, operation.firstValue, operation.secondValue);
+            calculatorDisplay.textContent = displayValue;
+            operation.firstValue = displayValue;
+            isbuttonPressed = true;
+            operation.secondValue = undefined;
+            displayValue = '0';
+        }  
+        if(b.textContent == '+') {
+            operation.operator = 'add';
+        }
+        else if(b.textContent == '-') {
+            operation.operator = 'sub';
+        }
+        else if(b.textContent == '*') {
+            operation.operator = 'mul';
+        }
+        else {
+            operation.operator = 'div';
+        }
+        
+        displayValue = "";
+        isbuttonPressed = true;
+        
+    });
 })
 
-equals.addEventListener('click', () => {
-    calculatorDisplay.textContent = operate("add", firstValue, lastValue);
-    isfirstNumber = true;
+
+
+// Equal Button functionality
+equalBtn.addEventListener('click', () => {
+    if(operation.firstValue !== undefined && operation.secondValue !== undefined) {
+        displayValue = operate(operation.operator, operation.firstValue, operation.secondValue);
+        calculatorDisplay.textContent = displayValue;
+        isbuttonPressed= false;
+        displayValue = "0";
+        operation = {firstValue: undefined, operator: undefined, secondValue: undefined};
+
+
+    }  
+});
+
+// clears and reset everything
+clearBtn.addEventListener('click', ()=> {
+    displayValue = "0";
+    isbuttonPressed = false;
+    calculatorDisplay.textContent = displayValue
+    operation = {firstValue: undefined, operator: undefined, secondValue: undefined};
 })
 
-clear.addEventListener('click', () => {
-    calculatorDisplay.textContent = 0;
-    firstValue = 0;
-    lastValue = 0;
-})
 
 
 
 
 
 
-
-
+// // Power Button 
+// let isPowerButtonClick = true;
+// powerButton.addEventListener('click', () => {
+//     if(isPowerButtonClick === true) {
+//         calculatorDisplay.setAttribute('style', 'background-color: #8d99ae;');
+//         buttons.setAttribute('style', 'background-color: #8d99ae;');
+//         calcBody.setAttribute('style', 'background-color: #8d99ae;');
+//         isPowerButtonClick = false;
+//     }
+//     else {
+//         calculatorDisplay.style.backgroundColor = "";
+//         buttons.style.backgroundColor = "";
+//         calcBody.style.backgroundColor = "";
+//         isPowerButtonClick = true;
+//     }
+// })
 
 
